91a8f457b89204d995bb6db2590e9552af077214
Gustavo <gustavo_s.alves@icloud.com>
2021-07-06 16:43:52 -0300

    Stripe SSG API Consuming

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SignInButton } from '../SignInButton';
+
+import styles from './styles.module.scss';
+
+export default function Header(){
+  return (
+      <header className={styles.headerContainer}>
+          <div className={styles.headerContent}>
+              <img src="/images/logo.svg" alt=""/>
+              <nav>
+                  <a className={styles.active} href="">Home</a>
+                  <a href="">Posts</a>
+              </nav>
+              <SignInButton />
+          </div>
+      </header>
+  )
+}
+
