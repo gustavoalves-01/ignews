a55e72b91665f8c4e680377ee8052b566b7eb950
Gustavo <gustavo_s.alves@icloud.com>
2022-02-17 00:18:51 -0300

    🧪  Testing ActiveLink, Header and SignInButton

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -2,24 +2,25 @@ import { ReactElement, cloneElement } from 'react';
 import Link, { LinkProps } from 'next/link';
 import { useRouter } from 'next/router';
 
-
 interface ActiveLinkProps extends LinkProps {
   children: ReactElement;
   activeClassName: string;
 }
 
-export function ActiveLink({children, activeClassName, ...rest}: ActiveLinkProps){
-  const {asPath} = useRouter();
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
 
-  const className = asPath === rest.href
-    ? activeClassName
-    : '';
+  const className = asPath === rest.href ? activeClassName : '';
 
   return (
     <Link {...rest}>
       {cloneElement(children, {
-        className
+        className,
       })}
     </Link>
-  )
+  );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import { SignInButton } from '../SignInButton';
-import {ActiveLink} from '../ActiveLink';
+import { ActiveLink } from '../ActiveLink';
 
 import styles from './styles.module.scss';
 
-export default function Header(){
+export function Header() {
   return (
-      <header className={styles.headerContainer}>
-          <div className={styles.headerContent}>
-              <img src="/images/logo.svg" alt=""/>
-              <nav>
-                <ActiveLink activeClassName={styles.active} href="/"> 
-                  <a>Home</a>
-                </ActiveLink>
-                <ActiveLink activeClassName={styles.active} href="/posts">
-                  <a>Posts</a>
-                </ActiveLink>
-              </nav>
-              <SignInButton />
-          </div>
-      </header>
-  )
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="" />
+        <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
-
